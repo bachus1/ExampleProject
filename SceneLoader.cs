cb88d7b2dee0e8fb480650ff7382265bf68887f6
bachus1 <121947133+bachus1@users.noreply.github.com>
2023-02-21 21:16:18 +0100

    Add files via upload

---
+++ SceneLoader.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace utdb_ChangingScenes1
+{
+    public class SceneLoader : MonoBehaviour
+    {
+
+        static bool sceneFlag = false;
+        public Button button;
+
+        private void Start()
+        {
+            button.onClick.AddListener(OnButtonClickHandler);
+        }
+
+        private void OnButtonClickHandler()
+        {
+            sceneFlag = !sceneFlag;
+            int sceneIndexToLoad = GetSceneIndex();
+            SceneManager.LoadScene(sceneIndexToLoad);
+        }
+
+        private static int GetSceneIndex()
+        {
+            if (sceneFlag)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
+
+
+
